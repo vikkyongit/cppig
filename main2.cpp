19e0e0d22095eb305d6743c3685d0ebea91f1ac1
Vikas Kumar Singh <79986419+vikkyongit@users.noreply.github.com>
2021-06-22 23:06:22 +0530

    Add files via upload

---
+++ main2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout <<"hi this is running "<< endl;
+    int number1;
+    int number2;
+    int number3;
+    int x;
+    int w;
+
+    cout<<" enter the 1st number"<<endl;
+    cin >> number1;
+
+    cout<<" enter the 2nd number"<<endl;
+    cin >> number2;
+    cout <<" enter the 3rd number"<<endl;
+    cin >>number3 ;
+    cout <<" enter the 4rd number"<<endl;
+    cin >>x;
+    cout <<" enter the 5rd number"<<endl;
+    cin >>w;
+
+
+     cout << " the 1st number is  "<< number1 << endl;
+
+     cout << " the 2nd number is  "<< number2 << endl;
+
+     cout << " the 3rd number is  "<< number3 << endl;
+
+     cout << " the x number is  "<< x << endl;
+
+      cout << " the w number is  "<< w<< endl;
+
+
+
+
+
+
+    return 0;
+}
