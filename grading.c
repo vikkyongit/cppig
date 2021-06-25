dcc0d3fad89bf9d537b5c6d33d6678570aded9f4
Vikas Kumar Singh <79986419+vikkyongit@users.noreply.github.com>
2021-06-25 23:36:43 +0530

    Add files via upload

---
+++ grading.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+    {
+        int s;
+        printf(" enter the value from 1 to 100\n");
+        scanf("%d",&s);
+        while(s>=0 && s<=100)
+        {
+            if (s > 39 && s<50)
+                printf(" PASS \n");
+        }
+        else
+            {
+    }
