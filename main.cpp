a9ab9993f4b0ceb2246a11e760f1e79ed0a9fd4f
Vikas Kumar Singh <79986419+vikkyongit@users.noreply.github.com>
2021-06-29 21:26:30 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<list>
+
+using namespace std;
+class YouTubeChannel{
+    public:
+    cout << "classCreating" << endl;
+    string Name;
+    int Subcribers;
+    list<string> PublishedVideoTitel;
+    cout <<"ClassCreated" <<endl;
+    }
+
+int main()
+
+{
+    cout << "InTheMainProgram" << endl;
+    YouTubeChannel channel;
+    channel.Name = "CodeMe";
+    channel.Subcribers = 100000;
+    channel.PublishedVideoTitel = {"A","B","c"};
+
+    cout
+
+
+
+         cout << "Hello world!" << endl;
+    return 0;
+}
